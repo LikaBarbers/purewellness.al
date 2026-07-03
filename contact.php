4ec9fe5335eafc5e097f069c2354811f5c2df6d1
LikaBarbers <xhuljanolika1@gmail.com>
2026-07-03 18:42:16 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,1 +1,77 @@
+<?php include "includes/header.php"; ?>
 
+<div class="container py-5">
+
+<h1 class="mb-5 text-center">
+
+Na Kontaktoni
+
+</h1>
+
+<div class="row">
+
+<div class="col-md-6">
+
+<h4>Informacion</h4>
+
+<p>
+
+📍 Rruga Osman Myderizi<br>
+
+Komuna e Parisit<br>
+
+Tiranë, Albania
+
+</p>
+
+<p>
+
+📞 +355 XX XXX XXXX
+
+</p>
+
+<p>
+
+✉ info@purewellness.al
+
+</p>
+
+</div>
+
+<div class="col-md-6">
+
+<form>
+
+<input
+class="form-control mb-3"
+placeholder="Emri">
+
+<input
+class="form-control mb-3"
+placeholder="Email">
+
+<input
+class="form-control mb-3"
+placeholder="Telefoni">
+
+<textarea
+class="form-control mb-3"
+rows="6"
+placeholder="Mesazhi"></textarea>
+
+<button
+class="btn btn-success">
+
+Dërgo
+
+</button>
+
+</form>
+
+</div>
+
+</div>
+
+</div>
+
+<?php include "includes/footer.php"; ?>
