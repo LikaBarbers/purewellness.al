53e5625b841e74653110c2118357f853e8a92d6c
LikaBarbers <xhuljanolika1@gmail.com>
2026-07-03 17:03:40 +0200

    Add utility functions for sanitization and redirection

--- includes/functions.php
+++ includes/functions.php
@@ -1,1 +1,19 @@
+<?php
 
+function sanitize($data)
+{
+    return htmlspecialchars(trim($data));
+}
+
+function redirect($url)
+{
+    header("Location: $url");
+    exit;
+}
+
+function isLoggedIn()
+{
+    return isset($_SESSION['admin']);
+}
+
+?>
