4da507b93a894dcc4990d21b0538228cd494b9fb
LikaBarbers <xhuljanolika1@gmail.com>
2026-07-03 18:32:33 +0200

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -1,1 +1,81 @@
+<?php
+include "includes/header.php";
+?>
 
+<div class="container py-5">
+
+<h2 class="mb-4">
+
+Përfundimi i Porosisë
+
+</h2>
+
+<form>
+
+<div class="row">
+
+<div class="col-md-6 mb-3">
+
+<label>Emri dhe Mbiemri</label>
+
+<input type="text" class="form-control">
+
+</div>
+
+<div class="col-md-6 mb-3">
+
+<label>Telefoni</label>
+
+<input type="text" class="form-control">
+
+</div>
+
+<div class="col-md-6 mb-3">
+
+<label>Email</label>
+
+<input type="email" class="form-control">
+
+</div>
+
+<div class="col-md-6 mb-3">
+
+<label>Qyteti</label>
+
+<input type="text" class="form-control">
+
+</div>
+
+<div class="col-12 mb-3">
+
+<label>Adresa</label>
+
+<textarea class="form-control"></textarea>
+
+</div>
+
+</div>
+
+<h4 class="mt-4">
+
+Metoda e Pagesës
+
+</h4>
+
+<div class="alert alert-success">
+
+Cash on Delivery (Paguani kur produkti të mbërrijë)
+
+</div>
+
+<button class="btn btn-success btn-lg">
+
+Dërgo Porosinë
+
+</button>
+
+</form>
+
+</div>
+
+<?php include "includes/footer.php"; ?>
