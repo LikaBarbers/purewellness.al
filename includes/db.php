8d33282a891a5420f14108211aa413d2aae36607
LikaBarbers <xhuljanolika1@gmail.com>
2026-07-03 17:03:10 +0200

    Add database connection setup in db.php

--- includes/db.php
+++ includes/db.php
@@ -1,1 +1,21 @@
+<?php
 
+require_once 'config.php';
+
+try {
+
+    $pdo = new PDO(
+        "mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=utf8",
+        DB_USER,
+        DB_PASS
+    );
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch(PDOException $e){
+
+    die("Database Connection Failed!");
+
+}
+
+?>
