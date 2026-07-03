fde7e7143d03bc77312b8e59ec8874eeb1cd09ff
LikaBarbers <xhuljanolika1@gmail.com>
2026-07-03 16:56:04 +0200

    Add footer with contact information and copyright

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,39 @@
+<footer class="bg-dark text-white mt-5 py-5">
 
+    <div class="container text-center">
+
+        <h4>Purewellness.al</h4>
+
+        <p>
+
+            Natural Wellness & Skincare
+
+        </p>
+
+        <p>
+
+            Rruga Osman Myderizi<br>
+
+            Komuna e Parisit<br>
+
+            Tiranë, Albania
+
+        </p>
+
+        <hr>
+
+        <p>
+
+            © <?php echo date('Y'); ?> Purewellness.al
+
+        </p>
+
+    </div>
+
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
