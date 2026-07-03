1a90ff3041bc13c1280e00c74c982a3ff20d8481
LikaBarbers <xhuljanolika1@gmail.com>
2026-07-03 18:41:33 +0200

    Update about.php

--- about.php
+++ about.php
@@ -1,1 +1,40 @@
 
+<?php include "includes/header.php"; ?>
+
+<div class="container py-5">
+
+    <div class="row align-items-center">
+
+        <div class="col-lg-6">
+
+            <h1 class="mb-4">Rreth Purewellness.al</h1>
+
+            <p>
+                Purewellness.al është një dyqan online i specializuar në produkte Wellness,
+                Skincare, Body Care dhe Hair Care.
+            </p>
+
+            <p>
+                Misioni ynë është të ofrojmë produkte cilësore që ndihmojnë njerëzit të
+                kujdesen për shëndetin dhe bukurinë e tyre.
+            </p>
+
+            <p>
+                Punojmë vetëm me marka të besueshme dhe produkte origjinale.
+            </p>
+
+        </div>
+
+        <div class="col-lg-6">
+
+            <img
+            src="assets/images/about.jpg"
+            class="img-fluid rounded shadow">
+
+        </div>
+
+    </div>
+
+</div>
+
+<?php include "includes/footer.php"; ?>
